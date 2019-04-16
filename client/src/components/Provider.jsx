7be0d815701d1b7c1bed6a827733ddd7dbd21e19
ankura <ankur_agrawal@intuit.com>
2019-04-16 16:08:58 -0700

    converted Provider component to full functional component

--- client/src/components/Provider.jsx
+++ client/src/components/Provider.jsx
@@ -12,61 +12,51 @@ import '../scss/Provider.scss';
 
 import provider from '../conf/provider.json';
 
-//TODO: convert this to functional component
-export default class Provider extends React.Component {
-    
-    constructor (props) {
-        super(props);
-        this.state = {        
-        };
-        this.loadProviderLogos();        
-    }
+const loadProviderLogos = () => {
+    provider.names.forEach(provider => {
+        if (provider.value.toLowerCase().includes('bofa')) {
+            provider.image = { avatar: true, src: BofaLogo };
+        }
+        else if (provider.value.toLowerCase().includes('citi')) {
+            provider.image = { avatar: true, src: CitiLogo };
+        }
+        else if (provider.value.toLowerCase().includes('amex')) {
+            provider.image = { avatar: true, src: AmexLogo };
+        }
+        else if (provider.value.toLowerCase().includes('comcast')) {
+            provider.image = { avatar: true, src: ComcastLogo };
+        }
+        else if (provider.value.toLowerCase().includes('fcu')) {
+            provider.image = { avatar: true, src: FirstTechLogo };
+        }
+        else if (provider.value.toLowerCase().includes('wf')) {
+            provider.image = { avatar: true, src: WellsFargoLogo };
+        }
+    })
+};
 
-    loadProviderLogos = () => {
-        provider.names.forEach(provider=>{
-            if (provider.value.toLowerCase().includes('bofa')) {
-                provider.image = {avatar:true,src:BofaLogo};
-            } 
-            else if (provider.value.toLowerCase().includes('citi')) {
-                provider.image = {avatar:true,src:CitiLogo};
-            }
-            else if (provider.value.toLowerCase().includes('amex')) {
-                provider.image = {avatar:true,src:AmexLogo};
-            }
-            else if (provider.value.toLowerCase().includes('comcast')) {
-                provider.image = {avatar:true,src:ComcastLogo};
-            }
-            else if (provider.value.toLowerCase().includes('fcu')) {
-                provider.image = {avatar:true,src:FirstTechLogo};
-            }
-            else if (provider.value.toLowerCase().includes('wf')) {
-                provider.image = {avatar:true,src:WellsFargoLogo};
-            }
-        })
-    };
-    
-    navigateBack = () => this.props.navigateBack()
+loadProviderLogos();
 
-    onChangeProvider = (event, data) => {                        
-        const providerName = data.value;
-        //render Accounts component
-        this.props.setRenderAccounts(true,providerName);        
-    }
+const onChangeProvider = (props,data) => {
+    const providerName = data.value;
+    //render Accounts component
+    props.setRenderAccounts(true, providerName);
+};
+
+const Provider = props => {
     
-    render() {             
-        return (
-            <div>
-                <Button color="black" style={{float:"right"}} icon="arrow alternate circle left" onClick={this.navigateBack} />
-                <div className="provider">
-                    <Dropdown placeholder='Choose your provider' fluid search selection options={provider.names} 
-                        onChange={this.onChangeProvider}/>
-                </div>
+    const navigateBack = () => props.navigateBack()
+
+    return (
+        <div>
+            <Button color="black" style={{ float: "right" }} icon="arrow alternate circle left" onClick={navigateBack} />
+            <div className="provider">
+                <Dropdown placeholder='Choose your provider' fluid search selection options={provider.names}
+                    onChange={(e,data) => onChangeProvider(props,data)} />
             </div>
-                            
-        )
-    }
-}
+        </div>
 
-//TODO: render Select provider 
-//TODO: render Menu to have all options like Create Account with default values, etc...
-//TODO:                         
+    );
+};
+
+export default Provider;
